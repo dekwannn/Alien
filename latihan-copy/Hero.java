b111610fd91c76335e1bbf68920c6d7e01687da6
widiadnyana002 <114465018+widiadnyana002@users.noreply.github.com>
2022-09-27 13:48:54 +0800

    Add files via upload

---
+++ latihan-copy/Hero.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        edge();
+    }
+    
+    public void edge(){
+        move(2);
+        
+        if (isAtEdge()){
+            setLocation(0, getY());
+        }
+
+    }
+}
